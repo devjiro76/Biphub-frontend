f5195868eff2ed35cdd2dfa008d61e4272e5808e
Jason Shin <visualbbasic@gmail.com>
2017-03-05 16:27:38 +1100

    initial commit

---
+++ client/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <counter></counter>
+    <p>
+      To get started, edit files in <code>./client</code> and save.
+      yea
+      {{ test }}
+    </p>
+  </div>
+</template>
+
+<script>
+import Counter from 'components/Counter'
+
+export default {
+  components: {
+    Counter
+  },
+  props: {
+  	test: {type: String, default: 'nothing!'}
+  }
+}
+</script>
